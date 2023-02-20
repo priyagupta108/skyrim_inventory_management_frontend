1f4a1151764b4ff5139edd1315d25d64b52d52f4
Dana Scheider <dana.scheider@gmail.com>
2023-02-21 09:13:38 +1100

    Update link to use value from paths object instead of hard-coded string

--- src/pages/loginPage/loginPage.tsx
+++ src/pages/loginPage/loginPage.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom'
+import paths from '../../routing/paths'
 import styles from './loginPage.module.css'
 
 const LoginPage = () => (
   <div className={styles.root}>
     <div className={styles.container}>
-      <Link className={styles.link} to="/">
+      <Link className={styles.link} to={paths.home}>
         Sign in with Google
       </Link>
     </div>
