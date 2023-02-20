435be41014de6ae134c0a4a2294e29cda6eafc16
Dana Scheider <dana.scheider@gmail.com>
2023-02-20 19:34:17 +1100

    Use new components in App component and update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import { describe, test, expect } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from './App'
 
 describe('<App />', () => {
@@ -9,12 +9,6 @@ describe('<App />', () => {
 
     // Get by h1
     const h1 = wrapper.container.querySelector('h1')
-    expect(h1?.textContent).toBe('Vite + React')
-
-    // Get by text using the React testing library
-    const text = screen.getByText(
-      /Click on the Vite and React logos to learn more/i
-    )
-    expect(text.textContent).toBeTruthy()
+    expect(h1?.textContent).toBe('Skyrim Inventory Management')
   })
 })
