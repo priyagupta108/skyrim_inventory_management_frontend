757c291a1571fd49909d2ab22bf2971fc682c496
Dana Scheider <dana.scheider@gmail.com>
2023-02-21 14:01:52 +1100

    Define colour schemes

---
+++ src/utils/colorSchemes.ts
@@ -0,0 +1,93 @@
+interface ColorScheme {
+  schemeColorDarkest: string
+  schemeColorDark: string
+  schemeColorLight: string
+  schemeColorLightest: string
+  hoverColorDark: string
+  hoverColorLight: string
+  hoverColorLightest: string
+  textColorPrimary: string
+  textColorSecondary: string
+  textColorTertiary: string
+  borderColor: string
+}
+
+export const YELLOW: ColorScheme = {
+  schemeColorDarkest: '#ffbf00',
+  schemeColorDark: '#ffcb32',
+  schemeColorLight: '#ffd866',
+  schemeColorLightest: '#fff2cc',
+  hoverColorDark: '#e5ab00',
+  hoverColorLight: '#ffc519',
+  hoverColorLightest: '#ffebb2',
+  textColorPrimary: '#4c3900',
+  textColorSecondary: '#4c3900',
+  textColorTertiary: '#7f5700',
+  borderColor: '#cc9800',
+}
+
+export const PINK: ColorScheme = {
+  schemeColorDarkest: '#e83f6f',
+  schemeColorDark: '#ec658b',
+  schemeColorLight: '#f18ba8',
+  schemeColorLightest: '#fad8e2',
+  hoverColorDark: '#d03863',
+  hoverColorLight: '#ea527d',
+  hoverColorLightest: '#f5b2c5',
+  textColorPrimary: '#fff',
+  textColorSecondary: '#fff',
+  textColorTertiary: '#451221',
+  borderColor: '#b93258',
+}
+
+export const BLUE: ColorScheme = {
+  schemeColorDarkest: '#2274a5',
+  schemeColorDark: '#4e8fb7',
+  schemeColorLight: '#7aabc9',
+  schemeColorLightest: '#d2e3ed',
+  hoverColorDark: '#1e6894',
+  hoverColorLight: '#3881ae',
+  hoverColorLightest: '#bcd5e4',
+  textColorPrimary: '#fff',
+  textColorSecondary: '#fff',
+  textColorTertiary: '#0d2e42',
+  borderColor: '#1b5c84'
+}
+
+export const GREEN: ColorScheme = {
+  schemeColorDarkest: '#00a323',
+  schemeColorDark: '#32b54e',
+  schemeColorLight: '#66c77b',
+  schemeColorLightest: '#ccecd3',
+  hoverColorDark: '#00921f',
+  hoverColorLight: '#19ac38',
+  hoverColorLightest: '#b2e3bd',
+  textColorPrimary: '#fff',
+  textColorSecondary: '#fff',
+  textColorTertiary: '#00410e',
+  borderColor: '#00821c'
+}
+
+export const AQUA: ColorScheme = {
+  schemeColorDarkest: '#20e2e9',
+  schemeColorDark: '#62eaef',
+  schemeColorLight: '#8ff0f4',
+  schemeColorLightest: '#d2f9fa',
+  hoverColorDark: '#11cbd1',
+  hoverColorLight: '#4ce7ed',
+  hoverColorLightest: '#bcf6f8',
+  textColorPrimary: '#094345',
+  textColorSecondary: '#094345',
+  textColorTertiary: '#094345',
+  borderColor: '#19b4ba'
+}
+
+const colorSchemes: ColorScheme[] = [
+  YELLOW,
+  PINK,
+  BLUE,
+  GREEN,
+  AQUA
+]
+
+export default colorSchemes
