7c8b6399b3583f42653bcd26f057058c6f6d1ce0
Dana Scheider <dana.scheider@gmail.com>
2023-02-25 08:40:45 +1100

    Add login button to the homepage (login page will be removed)

--- src/pages/homePage/homePage.tsx
+++ src/pages/homePage/homePage.tsx
@@ -1,16 +1,23 @@
-import { Link } from 'react-router-dom'
+import { type MouseEventHandler } from 'react'
+import GoogleSignInButton from '../../components/googleSignInButton/googleSignInButton'
 import paths from '../../routing/paths'
 import styles from './homePage.module.css'
 
-const HomePage = () => (
-  <div className={styles.root}>
-    <div className={styles.container}>
-      <h1 className={styles.header}>Skyrim Inventory Management</h1>
-      <Link className={styles.login} to={paths.login}>
-        Sign in with Google
-      </Link>
+const HomePage = () => {
+  const onClickLoginButton: MouseEventHandler = e => {
+    e.preventDefault()
+  }
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.container}>
+        <h1 className={styles.header}>Skyrim Inventory Management</h1>
+        <div className={styles.login}>
+          <GoogleSignInButton onClick={onClickLoginButton} />
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default HomePage
