b143f2ca30e5dbde77f7ae8ac17d9f24d0f0122e
Dana Scheider <dana.scheider@gmail.com>
2023-03-03 06:50:14 +1100

    Add UserInfo component for dashboard header with dropdown component for signout

---
+++ src/components/userInfo/userInfo.tsx
@@ -0,0 +1,50 @@
+import { type MouseEventHandler, useState } from 'react'
+import classnames from 'classnames'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { signOutWithGoogle } from '../../firebase'
+import { useGoogleLogin } from '../../hooks/contexts'
+import anonymousAvatar from './anonymousAvatar.jpg'
+import styles from './userInfo.module.css'
+
+const UserInfo = () => {
+  const [dropdownVisible, setDropdownVisible] = useState(false)
+  const { user } = useGoogleLogin()
+
+  const toggleDropdown: MouseEventHandler = (): void => {
+    setDropdownVisible(!dropdownVisible)
+  }
+
+  if (user) {
+    const { displayName, email } = user
+    const photoURL = user.photoURL
+
+    return (
+      <span className={styles.root}>
+        <div className={styles.main}>
+          <div className={styles.button} role='button' onClick={toggleDropdown}>
+            <span className={styles.info}>
+              <p className={styles.name}>{displayName}</p>
+              <p className={styles.email}>{email}</p>
+            </span>
+            <img className={styles.img} src={photoURL || anonymousAvatar} />
+          </div>
+        </div>
+        <menu className={classnames(styles.dropdown, { [styles.active]: dropdownVisible })}>
+          <div className={styles.signOut} onClick={signOutWithGoogle}>
+            <FontAwesomeIcon icon={faRightFromBracket} />
+            Sign Out
+          </div>
+        </menu>
+      </span>
+    )
+  } else {
+    return (
+      <div className={styles.main}>
+        <img className={styles.img} src={anonymousAvatar} />å
+      </div>
+    )
+  }
+}
+
+export default UserInfo
