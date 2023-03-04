0d956c86d09892011a36716fd6c6682c658f567a
Dana Scheider <dana.scheider@gmail.com>
2023-03-04 12:50:05 +1100

    Try fixing Firebase again

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePluginFonts } from 'vite-plugin-fonts'
-import replace from '@rollup/plugin-replace'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,20 +22,14 @@ export default defineConfig({
     }),
   ],
   build: {
-    rollupOptions: {
-      plugins: [
-        replace({
-          'process.env': JSON.stringify({
-            VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
-            VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-            VITE_FIREBASE_PROJECT_ID: process.env.VITE_FIREBASE_PROJECT_ID,
-            VITE_FIREBASE_STORAGE_BUCKET: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-            VITE_FIREBASE_MESSAGING_SENDER_ID: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-            VITE_FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID,
-          })
-        })
-      ]
-    }
+    env: {
+      VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
+      VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+      VITE_FIREBASE_PROJECT_ID: process.env.VITE_FIREBASE_PROJECT_ID,
+      VITE_FIREBASE_STORAGE_BUCKET: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+      VITE_FIREBASE_MESSAGING_SENDER_ID: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+      VITE_FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID,
+    },
   } as UserConfig['build'],
   test: {
     globals: true,
