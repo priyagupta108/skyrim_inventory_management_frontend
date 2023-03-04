ed6d7ca609226017408c58a7eb72d60e7dde4b31
Dana Scheider <dana.scheider@gmail.com>
2023-03-04 12:30:17 +1100

    Try again to fix Firebase deploys

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,3 +1,4 @@
+import dotenv from 'dotenv'
 import { initializeApp } from 'firebase/app'
 import {
   GoogleAuthProvider,
@@ -8,14 +9,16 @@ import {
   type UserCredential,
 } from 'firebase/auth'
 
+dotenv.config()
+
 // SIM's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
