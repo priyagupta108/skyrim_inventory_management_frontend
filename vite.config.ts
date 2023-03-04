b85e625f5c54bcb20308031faf4cf4491a6edaf0
Dana Scheider <dana.scheider@gmail.com>
2023-03-04 12:54:15 +1100

    Remove build section. Hopefully this time it fucking works

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types='vitest' />
-import { defineConfig, UserConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePluginFonts } from 'vite-plugin-fonts'
 
@@ -21,16 +21,6 @@ export default defineConfig({
       },
     }),
   ],
-  build: {
-    env: {
-      VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
-      VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-      VITE_FIREBASE_PROJECT_ID: process.env.VITE_FIREBASE_PROJECT_ID,
-      VITE_FIREBASE_STORAGE_BUCKET: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-      VITE_FIREBASE_MESSAGING_SENDER_ID: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-      VITE_FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID,
-    },
-  } as UserConfig['build'],
   test: {
     globals: true,
     environment: 'happy-dom',
