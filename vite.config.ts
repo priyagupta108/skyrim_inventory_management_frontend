034e001562c988e51e1019376a0ddf161cd04bc2
Dana Scheider <dana.scheider@gmail.com>
2023-03-04 12:44:55 +1100

    Attempt to fix error accessing process.env

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePluginFonts } from 'vite-plugin-fonts'
+import replace from '@rollup/plugin-replace'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,14 +23,20 @@ export default defineConfig({
     }),
   ],
   build: {
-    env: {
-      VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
-      VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-      VITE_FIREBASE_PROJECT_ID: process.env.VITE_FIREBASE_PROJECT_ID,
-      VITE_FIREBASE_STORAGE_BUCKET: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-      VITE_FIREBASE_MESSAGING_SENDER_ID: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-      VITE_FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID,
-    },
+    rollupOptions: {
+      plugins: [
+        replace({
+          'process.env': JSON.stringify({
+            VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
+            VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+            VITE_FIREBASE_PROJECT_ID: process.env.VITE_FIREBASE_PROJECT_ID,
+            VITE_FIREBASE_STORAGE_BUCKET: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+            VITE_FIREBASE_MESSAGING_SENDER_ID: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+            VITE_FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID,
+          })
+        })
+      ]
+    }
   } as UserConfig['build'],
   test: {
     globals: true,
