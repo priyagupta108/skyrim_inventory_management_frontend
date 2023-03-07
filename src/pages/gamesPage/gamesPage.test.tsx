4340dd0983e8541e4b4cadc8e3ad955280ae3907
Dana Scheider <dana.scheider@gmail.com>
2023-03-07 21:18:39 +1100

    Prepare test file for adding more tests on destroying games

--- src/pages/gamesPage/gamesPage.test.tsx
+++ src/pages/gamesPage/gamesPage.test.tsx
@@ -1,4 +1,4 @@
-import { describe, test, expect, beforeEach } from 'vitest'
+import { describe, test, expect, beforeEach, afterEach } from 'vitest'
 import { screen, waitFor } from '@testing-library/react'
 import { renderAuthenticated, renderAuthLoading } from '../../support/testUtils'
 import { emptyGames, allGames } from '../../support/data/games'
@@ -8,164 +8,170 @@ import { GamesProvider } from '../../contexts/gamesContext'
 import GamesPage from './gamesPage'
 
 describe('<GamesPage />', () => {
-  beforeEach(() => {
+  afterEach(() => {
     fetch.resetMocks()
   })
 
-  describe('when loading', () => {
-    test('displays the loader', () => {
-      const wrapper = renderAuthLoading(
-        <PageProvider>
-          <GamesProvider>
-            <GamesPage />
-          </GamesProvider>
-        </PageProvider>
-      )
-      expect(wrapper).toBeTruthy()
+  describe('viewing games', () => {
+    describe('when loading', () => {
+      test('displays the loader', () => {
+        const wrapper = renderAuthLoading(
+          <PageProvider>
+            <GamesProvider>
+              <GamesPage />
+            </GamesProvider>
+          </PageProvider>
+        )
+        expect(wrapper).toBeTruthy()
 
-      expect(screen.getByTestId('pulseLoader')).toBeTruthy()
-    })
+        expect(screen.getByTestId('pulseLoader')).toBeTruthy()
+      })
 
-    test('matches snapshot', () => {
-      const wrapper = renderAuthLoading(
-        <PageProvider>
-          <GamesProvider>
-            <GamesPage />
-          </GamesProvider>
-        </PageProvider>
-      )
+      test('matches snapshot', () => {
+        const wrapper = renderAuthLoading(
+          <PageProvider>
+            <GamesProvider>
+              <GamesPage />
+            </GamesProvider>
+          </PageProvider>
+        )
 
-      expect(wrapper).toMatchSnapshot()
-    })
-  })
-
-  describe('when there are no games', () => {
-    beforeEach(() => {
-      fetch.mockResponseOnce(JSON.stringify(emptyGames), { status: 200 })
-    })
-
-    test('games page displays a message that there are no games', async () => {
-      const wrapper = renderAuthenticated(
-        <PageProvider>
-          <GamesProvider>
-            <GamesPage />
-          </GamesProvider>
-        </PageProvider>
-      )
-      expect(wrapper).toBeTruthy()
-
-      await waitFor(() => {
-        expect(screen.findByText('You have no games.')).toBeTruthy()
-        expect(screen.queryByTestId('pulseLoader')).toBeFalsy()
+        expect(wrapper).toMatchSnapshot()
       })
     })
 
-    test('matches snapshot', () => {
-      const wrapper = renderAuthenticated(
-        <PageProvider>
-          <GamesProvider>
-            <GamesPage />
-          </GamesProvider>
-        </PageProvider>
-      )
-      expect(wrapper).toMatchSnapshot()
-    })
-  })
+    describe('when there are no games', () => {
+      beforeEach(() => {
+        fetch.mockResponseOnce(JSON.stringify(emptyGames), { status: 200 })
+      })
 
-  describe('when there are multiple games', () => {
-    beforeEach(() => {
-      fetch.mockResponseOnce(JSON.stringify(allGames), { status: 200 })
-    })
+      test('games page displays a message that there are no games', async () => {
+        const wrapper = renderAuthenticated(
+          <PageProvider>
+            <GamesProvider>
+              <GamesPage />
+            </GamesProvider>
+          </PageProvider>
+        )
+        expect(wrapper).toBeTruthy()
 
-    // Descriptions should be hidden by default but Vitest has no way of knowing
-    // that, as noted in the test file for the GameLineItem component.
-    test('displays the title and description of each game', async () => {
-      renderAuthenticated(
-        <PageProvider>
-          <GamesProvider>
-            <GamesPage />
-          </GamesProvider>
-        </PageProvider>
-      )
+        await waitFor(() => {
+          expect(screen.findByText('You have no games.')).toBeTruthy()
+          expect(screen.queryByTestId('pulseLoader')).toBeFalsy()
+        })
+      })
 
-      await waitFor(() => {
-        expect(screen.findByText('My Game 1')).toBeTruthy()
-        expect(screen.findByText('This is a game with a description')).toBeTruthy()
-
-        expect(screen.findByText('My Game 2')).toBeTruthy()
-        expect(screen.findByText('This game has no description.')).toBeTruthy()
-
-        expect(
-          screen.getByText(
-            'Game with a really really really really really long name'
-          )
-        ).toBeTruthy()
-        expect(
-          screen.getByText(
-            /Cum audissem Antiochum, Brute, ut solebam, cum M\. Pisone/
-          )
-        ).toBeTruthy()
-
-        expect(screen.queryByTestId('pulseLoader')).toBeFalsy()
-        expect(screen.queryByText('You have no games.')).toBeFalsy()
+      test('matches snapshot', () => {
+        const wrapper = renderAuthenticated(
+          <PageProvider>
+            <GamesProvider>
+              <GamesPage />
+            </GamesProvider>
+          </PageProvider>
+        )
+        expect(wrapper).toMatchSnapshot()
       })
     })
 
-    test('matches snapshot', () => {
-      const wrapper = renderAuthenticated(
-        <PageProvider>
-          <GamesProvider>
-            <GamesPage />
-          </GamesProvider>
-        </PageProvider>
-      )
-      expect(wrapper).toMatchSnapshot()
-    })
-  })
+    describe('when there are multiple games', () => {
+      beforeEach(() => {
+        fetch.mockResponseOnce(JSON.stringify(allGames), { status: 200 })
+      })
 
-  describe('when the server returns an error', () => {
-    beforeEach(() => {
-      fetch.mockResponseOnce(JSON.stringify(internalServerErrorResponse), {
-        status: 500,
+      // Descriptions should be hidden by default but Vitest has no way of knowing
+      // that, as noted in the test file for the GameLineItem component.
+      test('displays the title and description of each game', async () => {
+        renderAuthenticated(
+          <PageProvider>
+            <GamesProvider>
+              <GamesPage />
+            </GamesProvider>
+          </PageProvider>
+        )
+
+        await waitFor(() => {
+          expect(screen.findByText('My Game 1')).toBeTruthy()
+          expect(
+            screen.findByText('This is a game with a description')
+          ).toBeTruthy()
+
+          expect(screen.findByText('My Game 2')).toBeTruthy()
+          expect(
+            screen.findByText('This game has no description.')
+          ).toBeTruthy()
+
+          expect(
+            screen.getByText(
+              'Game with a really really really really really long name'
+            )
+          ).toBeTruthy()
+          expect(
+            screen.getByText(
+              /Cum audissem Antiochum, Brute, ut solebam, cum M\. Pisone/
+            )
+          ).toBeTruthy()
+
+          expect(screen.queryByTestId('pulseLoader')).toBeFalsy()
+          expect(screen.queryByText('You have no games.')).toBeFalsy()
+        })
+      })
+
+      test('matches snapshot', () => {
+        const wrapper = renderAuthenticated(
+          <PageProvider>
+            <GamesProvider>
+              <GamesPage />
+            </GamesProvider>
+          </PageProvider>
+        )
+        expect(wrapper).toMatchSnapshot()
       })
     })
 
-    test('displays error content', async () => {
-      renderAuthenticated(
-        <PageProvider>
-          <GamesProvider>
-            <GamesPage />
-          </GamesProvider>
-        </PageProvider>
-      )
-
-      await waitFor(() => {
-        expect(screen.findByText('500 Internal Server Error')).toBeTruthy()
+    describe('when the server returns an error', () => {
+      beforeEach(() => {
+        fetch.mockResponseOnce(JSON.stringify(internalServerErrorResponse), {
+          status: 500,
+        })
       })
-    })
 
-    test("doesn't break the dashboard", () => {
-      renderAuthenticated(
-        <PageProvider>
-          <GamesProvider>
-            <GamesPage />
-          </GamesProvider>
-        </PageProvider>
-      )
+      test('displays error content', async () => {
+        renderAuthenticated(
+          <PageProvider>
+            <GamesProvider>
+              <GamesPage />
+            </GamesProvider>
+          </PageProvider>
+        )
 
-      expect(screen.getByText('Your Games')).toBeTruthy()
-    })
+        await waitFor(() => {
+          expect(screen.findByText('500 Internal Server Error')).toBeTruthy()
+        })
+      })
 
-    test('matches snapshot', () => {
-      const wrapper = renderAuthenticated(
-        <PageProvider>
-          <GamesProvider>
-            <GamesPage />
-          </GamesProvider>
-        </PageProvider>
-      )
+      test("doesn't break the dashboard", () => {
+        renderAuthenticated(
+          <PageProvider>
+            <GamesProvider>
+              <GamesPage />
+            </GamesProvider>
+          </PageProvider>
+        )
 
-      expect(wrapper).toMatchSnapshot()
+        expect(screen.getByText('Your Games')).toBeTruthy()
+      })
+
+      test('matches snapshot', () => {
+        const wrapper = renderAuthenticated(
+          <PageProvider>
+            <GamesProvider>
+              <GamesPage />
+            </GamesProvider>
+          </PageProvider>
+        )
+
+        expect(wrapper).toMatchSnapshot()
+      })
     })
   })
 })
