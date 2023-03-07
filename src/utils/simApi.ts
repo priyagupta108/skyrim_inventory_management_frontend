6a2b945b623cddb0c44300bc30fcfd58e1a44300
Dana Scheider <dana.scheider@gmail.com>
2023-03-07 22:42:08 +1100

    Modify getGames function to provide more detailed error messages for 500 errors

--- src/utils/simApi.ts
+++ src/utils/simApi.ts
@@ -1,4 +1,5 @@
 import { type Game } from '../types/games'
+import { type ErrorObject } from '../types/apiData'
 import {
   AuthorizationError,
   InternalServerError,
@@ -16,6 +17,13 @@ const combinedHeaders = (token: string) => ({
   ...authHeader(token),
 })
 
+/*
+ *
+ * Game Endpoints
+ *
+ */
+
+// GET /games
 export const getGames = (token: string) => {
   const uri = `${baseUri}/games`
   const headers = combinedHeaders(token)
@@ -23,6 +31,11 @@ export const getGames = (token: string) => {
   return fetch(uri, { headers }).then((res: Response) => {
     if (res.status === 401) throw new AuthorizationError()
     if (res.status === 500) throw new InternalServerError()
-    return res.json().then((json: Game[]) => ({ status: res.status, json }))
+    return res.json().then((json: Game[] | ErrorObject) => {
+      if (res.status === 500 && typeof json === 'object' && 'errors' in json)
+        throw new AuthorizationError(json.errors.join('\n'))
+
+      return { status: res.status, json }
+    })
   })
 }
