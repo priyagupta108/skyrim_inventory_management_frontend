545136e4c8f50e57f36bece2545c94b901d4669e
Dana Scheider <dana.scheider@gmail.com>
2023-03-16 12:16:35 +1100

    Create the StyledSelectOption component that will be used for the games dropdown

---
+++ src/components/styledSelectOption/styledSelectOption.test.tsx
@@ -0,0 +1,109 @@
+import { type KeyboardEventHandler, type MouseEventHandler } from 'react'
+import { describe, test, expect, beforeEach } from 'vitest'
+import { act, fireEvent } from '@testing-library/react'
+import { render } from '../../support/testUtils'
+import StyledSelectOption from './styledSelectOption'
+
+describe('StyledSelectOption', () => {
+  describe('displaying the option', () => {
+    describe('when not selected', () => {
+      describe('when the option name is shorter than 24 characters', () => {
+        test('displays the option name and has aria-selected set to "false"', () => {
+          const wrapper = render(
+            <StyledSelectOption
+              optionName="Option 1"
+              onClick={() => {}}
+              onKeyDown={() => {}}
+              ariaSelected={false}
+            />
+          )
+
+          expect(wrapper).toBeTruthy()
+
+          const option = wrapper.getByRole('option', { selected: false })
+
+          expect(option).toBeTruthy()
+          expect(option.textContent).toEqual('Option 1')
+        })
+      })
+
+      describe('when the option name is longer than 24 characters', () => {
+        test('truncates the name to 24 characters', () => {
+          const wrapper = render(
+            <StyledSelectOption
+              optionName="Neque porro quisquam est quis dolorem ipsum quia dolor sit amet"
+              onClick={() => {}}
+              onKeyDown={() => {}}
+              ariaSelected={false}
+            />
+          )
+
+          const option = wrapper.getByRole('option', { selected: false })
+
+          expect(option).toBeTruthy()
+          expect(option.textContent).toEqual('Neque porro quisquam es...')
+        })
+      })
+    })
+
+    describe('when selected', () => {
+      test('displays the option name and has aria-selected set to "false"', () => {
+        const wrapper = render(
+          <StyledSelectOption
+            optionName="Option 1"
+            onClick={() => {}}
+            onKeyDown={() => {}}
+            ariaSelected={true}
+          />
+        )
+
+        expect(wrapper).toBeTruthy()
+
+        const option = wrapper.getByRole('option', { selected: true })
+
+        expect(option).toBeTruthy()
+        expect(option.textContent).toEqual('Option 1')
+      })
+    })
+  })
+
+  describe('event handling', () => {
+    test('calls the onClick function when clicked', () => {
+      const onClick = vitest.fn() as MouseEventHandler
+
+      const wrapper = render(
+        <StyledSelectOption
+          optionName="Option 1"
+          onClick={onClick}
+          onKeyDown={() => {}}
+          ariaSelected={false}
+        />
+      )
+
+      const option = wrapper.getByRole('option')
+
+      act(() => option.click())
+
+      expect(onClick).toHaveBeenCalledOnce()
+    })
+
+    test('calls the onKeyDown function when a key is pressed over it', () => {
+      const onKeyDown = vitest.fn() as KeyboardEventHandler
+
+      const wrapper = render(
+        <StyledSelectOption
+          optionName="Option 1"
+          onClick={() => {}}
+          onKeyDown={onKeyDown}
+          ariaSelected={false}
+        />
+      )
+
+      const option = wrapper.getByRole('option')
+
+      act(() => fireEvent.keyDown(option))
+
+      expect(onKeyDown).toHaveBeenCalledOnce()
+    })
+  })
+})
---
+++ src/components/styledSelectOption/styledSelectOption.tsx
@@ -0,0 +1,36 @@
+import { type CSSProperties, type KeyboardEventHandler, type MouseEventHandler } from 'react'
+import classNames, { type Argument } from 'classnames'
+import styles from './styledSelectOption.module.css'
+
+interface StyledSelectOptionProps {
+  optionName: string
+  onClick: MouseEventHandler
+  onKeyDown: KeyboardEventHandler
+  ariaSelected: boolean
+  className?: Argument
+}
+
+const StyledSelectOption = ({
+  optionName,
+  onClick,
+  onKeyDown,
+  ariaSelected,
+  className,
+}: StyledSelectOptionProps) => {
+  const displayName = () => (
+    optionName.length > 24 ? `${optionName.substring(0, 23).trim()}...` : optionName
+  )
+
+  return (
+    <li
+      className={classNames(styles.root, className)}
+      onClick={onClick}
+      onKeyDown={onKeyDown}
+      role="option"
+      tabIndex={0}
+      aria-selected={ariaSelected}
+    >{displayName()}</li>
+  )
+}
+
+export default StyledSelectOption
