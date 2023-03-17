a3a36bc9830539916ab3f82776abdfac34020cbf
Dana Scheider <dana.scheider@gmail.com>
2023-03-18 09:56:35 +1100

    Add MSW handlers for shopping list creation in preparation for testing of shopping list creation

---
+++ src/support/msw/helpers/data.ts
@@ -0,0 +1,86 @@
+import {
+  type RequestShoppingList,
+  type ResponseShoppingList,
+} from '../../../types/apiData'
+import { allGames } from '../../data/games'
+import { shoppingListsForGame } from '../../data/shoppingLists'
+
+const gameIds = allGames.map(({ id }) => id)
+
+/**
+ *
+ * Shopping List Creation
+ *
+ */
+
+export const newShoppingList = (
+  attributes: RequestShoppingList,
+  gameId: number
+): ResponseShoppingList[] => {
+  if (gameIds.indexOf(gameId) < 0)
+    throw new Error(
+      'Cannot generate shopping list for game that does not exist in test data'
+    )
+
+  const existingLists = shoppingListsForGame(gameId)
+
+  if (!existingLists.length)
+    throw new Error(
+      'Cannot generate single list for game without existing aggregate'
+    )
+
+  const newList = {
+    id: 93,
+    game_id: gameId,
+    aggregate: false,
+    aggregate_list_id: existingLists[0].id,
+    title: attributes.title || 'New Shopping List',
+    list_items: [],
+    created_at: new Date(),
+    updated_at: new Date(),
+  }
+
+  existingLists.splice(1, 0, newList)
+
+  return existingLists
+}
+
+export const newShoppingListWithAggregate = (
+  attributes: RequestShoppingList,
+  gameId: number
+): ResponseShoppingList[] => {
+  if (gameIds.indexOf(gameId) < 0)
+    throw new Error(
+      'Cannot generate shopping list for game that does not exist in test data'
+    )
+
+  const existingLists = shoppingListsForGame(gameId)
+
+  if (existingLists.length)
+    throw new Error(
+      'Cannot generate new aggregate list for game that already has one'
+    )
+
+  return [
+    {
+      id: 93,
+      game_id: gameId,
+      aggregate: true,
+      aggregate_list_id: null,
+      title: 'All Items',
+      list_items: [],
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      id: 94,
+      game_id: gameId,
+      aggregate: false,
+      aggregate_list_id: 93,
+      title: attributes.title || 'My Shopping List 1',
+      list_items: [],
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+  ]
+}
--- src/support/msw/shoppingLists.ts
+++ src/support/msw/shoppingLists.ts
@@ -1,9 +1,67 @@
 import { rest } from 'msw'
-import { type ResponseShoppingList } from '../../types/apiData'
+import {
+  type RequestShoppingList,
+  type ResponseShoppingList,
+} from '../../types/apiData'
 import { allGames } from '../data/games'
-import { allShoppingLists, shoppingListsForGame } from '../data/shoppingLists'
+import { shoppingListsForGame } from '../data/shoppingLists'
+import { newShoppingList, newShoppingListWithAggregate } from './helpers/data'
 
 const BASE_URI = 'http://localhost:3000'
+const gameIds = allGames.map(({ id }) => id)
+
+/**
+ *
+ * POST /games/:game_id/shopping_lists
+ *
+ */
+
+// Handles both 201 and 404 responses
+export const postShoppingList = rest.post(
+  `${BASE_URI}/games/:gameId/shopping_lists`,
+  async (req, res, ctx) => {
+    const gameId = Number(req.params.gameId)
+
+    if (gameIds.indexOf(gameId) < 0) return res(ctx.status(404))
+
+    const attributes = await req.json()
+
+    const responseBody = shoppingListsForGame(gameId).length
+      ? newShoppingList(attributes, gameId)
+      : newShoppingListWithAggregate(attributes, gameId)
+
+    return res(ctx.status(201), ctx.json(responseBody))
+  }
+)
+
+// Returns the same validation errors regardless of request body
+// submitted
+export const postShoppingListUnprocessable = rest.post(
+  `${BASE_URI}/games/:gameId/shopping_lists`,
+  (_req, res, ctx) => {
+    return res(
+      ctx.status(422),
+      ctx.json({
+        errors: [
+          'Title must be unique',
+          "Title can only contain alphanumeric characters, spaces, commas (,), hyphens (-), and apostrophes (')",
+        ],
+      })
+    )
+  }
+)
+
+export const postShoppingListServerError = rest.post(
+  `${BASE_URI}/games/:gameId/shopping_lists`,
+  (_req, res, ctx) => {
+    return res(
+      ctx.status(500),
+      ctx.json({
+        errors: ['Something went horribly wrong'],
+      })
+    )
+  }
+)
 
 /**
  *
