3fdb60859562627a69475b11b2277bb49b5e4d94
Dana Scheider <dana.scheider@gmail.com>
2023-03-28 16:46:08 +1100

    Basic list edit form component to go in shopping/inventory list headers

---
+++ src/components/listEditForm/listEditForm.test.tsx
@@ -0,0 +1,75 @@
+import { useRef, type FormEvent, type FormEventHandler } from 'react'
+import { describe, test, expect, vitest } from 'vitest'
+import { act, fireEvent } from '@testing-library/react'
+import { render } from '../../support/testUtils'
+import { BLUE } from '../../utils/colorSchemes'
+import { ColorProvider } from '../../contexts/colorContext'
+import ListEditForm from './listEditForm'
+
+interface WrapperProps {
+  onSubmit: FormEventHandler
+}
+
+const WrapperComponent = ({ onSubmit }: WrapperProps) => {
+  const formRef = useRef(null)
+
+  return (
+    <div>
+      <ListEditForm
+        className="foo"
+        formRef={formRef}
+        maxTotalWidth={256}
+        title="Alchemy Ingredients"
+        onSubmit={onSubmit}
+      />
+    </div>
+  )
+}
+
+describe('ListEditForm', () => {
+  test('displays the title and submit button', () => {
+    const wrapper = render(
+      <WrapperComponent onSubmit={(e: FormEvent) => e.preventDefault()} />
+    )
+
+    expect(wrapper).toBeTruthy()
+    expect(
+      wrapper.getByRole('textbox').attributes.getNamedItem('name')
+    ).toEqual('title')
+    expect(wrapper.getByRole('button').attributes.getNamedItem('name')).toEqual(
+      'submit'
+    )
+  })
+
+  describe('submitting the form', () => {
+    test('calls the onSubmit function passed in when button is clicked', () => {
+      const onSubmit = vitest.fn()
+      const wrapper = render(<WrapperComponent onSubmit={onSubmit} />)
+
+      const input = wrapper.getByRole('textbox')
+      const button = wrapper.getByRole('button')
+
+      act(() => {
+        fireEvent.change(input, { target: { value: 'Smithing Materials' } })
+        fireEvent.click(button)
+      })
+
+      expect(onSubmit).toHaveBeenCalledOnce()
+    })
+
+    test('calls the onSubmit function any time the form is submitted', () => {
+      const onSubmit = vitest.fn()
+      const wrapper = render(<WrapperComponent onSubmit={onSubmit} />)
+
+      const input = wrapper.getByRole('textbox')
+      const form = wrapper.getByRole('form')
+
+      act(() => {
+        fireEvent.change(input, { target: { value: 'Smithing Materials' } })
+        fireEvent.submit(form)
+      })
+
+      expect(onSubmit).toHaveBeenCalledOnce()
+    })
+  })
+})
---
+++ src/components/listEditForm/listEditForm.tsx
@@ -0,0 +1,123 @@
+import {
+  useState,
+  useRef,
+  useEffect,
+  type FormEventHandler,
+  type CSSProperties,
+  type ChangeEvent,
+  type LegacyRef,
+} from 'react'
+import classNames from 'classnames'
+import { useColorScheme } from '../../hooks/contexts'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareCheck } from '@fortawesome/free-regular-svg-icons'
+import styles from './listEditForm.module.css'
+
+interface EditFormProps {
+  formRef: LegacyRef<HTMLFormElement>
+  maxTotalWidth: number
+  className?: string
+  title: string
+  onSubmit: FormEventHandler
+}
+
+const ListEditForm = ({
+  formRef,
+  maxTotalWidth,
+  className,
+  title,
+  onSubmit,
+}: EditFormProps) => {
+  const [maxTextWidth, setMaxTextWidth] = useState<number | null>(null)
+
+  const getInputTextWidth = (text: string) => {
+    const canvas = document.createElement('canvas')
+    const context = canvas.getContext('2d')
+
+    if (!context) return 0
+
+    context.font = '21px Quattrocento Sans'
+
+    const max = maxTextWidth || maxTotalWidth
+
+    const textWidth = context.measureText(text).width
+
+    return Math.min(textWidth, max)
+  }
+
+  const [inputValue, setInputValue] = useState(title)
+  const [inputWidth, setInputWidth] = useState(
+    `${getInputTextWidth(inputValue)}px`
+  )
+
+  const inputRef = useRef<HTMLInputElement>(null)
+  const buttonRef = useRef<HTMLButtonElement>(null)
+
+  const {
+    schemeColorDarkest,
+    textColorPrimary,
+    borderColor,
+    schemeColorLightest,
+  } = useColorScheme()
+
+  const rootStyles = {
+    '--scheme-color': schemeColorDarkest,
+    '--text-color': textColorPrimary,
+    '--border-color': borderColor,
+    '--icon-hover-color': schemeColorLightest,
+    '--max-width': `${maxTotalWidth}px`,
+  } as CSSProperties
+
+  const updateInputWidth = (e: ChangeEvent) => {
+    const newValue = (e.currentTarget as HTMLInputElement)?.value || ''
+    setInputValue(newValue)
+    setInputWidth(`${getInputTextWidth(newValue)}px`)
+  }
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
+
+  useEffect(() => {
+    setMaxTextWidth(
+      buttonRef.current
+        ? maxTotalWidth - buttonRef.current.offsetWidth
+        : maxTotalWidth
+    )
+    setInputWidth(`${getInputTextWidth(inputValue)}px`)
+  }, [maxTotalWidth])
+
+  return (
+    <form
+      className={classNames(className, styles.root)}
+      style={rootStyles}
+      ref={formRef}
+      onSubmit={onSubmit}
+      aria-label="List title edit form"
+    >
+      <input
+        className={styles.input}
+        onClick={(e) => e.stopPropagation()}
+        onChange={updateInputWidth}
+        type="text"
+        name="title"
+        aria-label="title"
+        ref={inputRef}
+        style={{ width: inputWidth }}
+        defaultValue={inputValue}
+        pattern="^\s*[A-Za-z0-9 \-',]*\s*$"
+        title="Title can only contain alphanumeric characters, spaces, hyphens, commas, and apostrophes"
+      />
+      <button
+        className={styles.submit}
+        ref={buttonRef}
+        name="submit"
+        type="submit"
+      >
+        <FontAwesomeIcon className={styles.fa} icon={faSquareCheck} />
+      </button>
+    </form>
+  )
+}
+
+export default ListEditForm
