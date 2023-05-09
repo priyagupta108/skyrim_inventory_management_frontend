9635ac59e0c0755c4c525d51da79a35eac5a8a70
Dana Scheider <dana.scheider@gmail.com>
2023-05-10 08:26:57 +1000

    Attempt to fix broken test in CI

--- src/pages/shoppingListsPage/shoppingListsPage.test.tsx
+++ src/pages/shoppingListsPage/shoppingListsPage.test.tsx
@@ -6,6 +6,7 @@ import {
   beforeEach,
   afterAll,
   afterEach,
+  vitest,
 } from 'vitest'
 import {
   waitFor,
@@ -343,11 +344,13 @@ describe('ShoppingListsPage', () => {
           'http://localhost:5173/shopping_lists'
         )
 
-        await wrapper.findByText(
+        await  wrapper.findByText(
           /You need a game to use the shopping lists feature\./
         )
 
-        expect(wrapper).toMatchSnapshot()
+        await waitFor(() => {
+          expect(wrapper).toMatchSnapshot()
+        })
       })
     })
   })
