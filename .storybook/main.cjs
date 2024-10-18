f37eaabe1b8f746282cf071c81e67f777aed2c76
Dana Scheider <dana.scheider@gmail.com>
2024-10-19 04:14:26 +1100

    Update to Storybook 8

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,20 +1,23 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     'storybook-addon-mock',
     '@storybook/addon-mdx-gfm',
+    '@chromatic-com/storybook'
   ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
-  features: {
-    storyStoreV7: true,
-  },
-  docs: {
-    autodocs: true,
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 }
